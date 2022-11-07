ababcd1ae4a55edd8983fb326ebdd2c3a1108d97
jon-cron <jonathanacm95@gmail.com>
2022-11-07 14:58:38 -0700

    made js files for my controller and services

--- app/AppState.js
+++ app/AppState.js
@@ -1,22 +1,24 @@
-import { Value } from "./Models/Value.js"
-import { EventEmitter } from "./Utils/EventEmitter.js"
-import { isValidProp } from "./Utils/isValidProp.js"
-import { loadState } from "./Utils/Store.js"
+import { Value } from "./Models/Value.js";
+import { EventEmitter } from "./Utils/EventEmitter.js";
+import { isValidProp } from "./Utils/isValidProp.js";
+import { loadState } from "./Utils/Store.js";
 
 class AppState extends EventEmitter {
   /** @type {import('./Models/Value').Value[]} */
-  values = loadState('values', [Value])
+  values = loadState("values", [Value]);
 }
 
 export const appState = new Proxy(new AppState(), {
   get(target, prop) {
-    isValidProp(target, prop)
-    return target[prop]
+    isValidProp(target, prop);
+    return target[prop];
   },
   set(target, prop, value) {
-    isValidProp(target, prop)
-    target[prop] = value
-    target.emit(prop, value)
-    return true
-  }
-})
+    isValidProp(target, prop);
+    target[prop] = value;
+    target.emit(prop, value);
+    return true;
+  },
+});
+
+characters = [new Character("Noah", 0), new Character("Bob", 0)];
